676c9ead3bfe7ce61c7a37791188b16c6479824a
Ganesh <48731886+GaneshSparkz@users.noreply.github.com>
2020-08-24 13:31:02 +0530

    Driver code

---
+++ demo.py
@@ -0,0 +1,28 @@
+from utils.sudoku import Sudoku
+
+
+# initialize the 9x9 puzzle board
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7],
+]
+
+# instantiate the Sudoku puzzle
+puzzle = Sudoku(board, n_rows=9, n_cols=9)
+
+print("[INFO] The given Sudoku puzzle...")
+puzzle.print_board()
+
+print("[INFO] Solving the puzzle...")
+puzzle.solve()
+print("[INFO] Solved...")
+
+print("[INFO] The solution of the puzzle is...")
+puzzle.print_board()
